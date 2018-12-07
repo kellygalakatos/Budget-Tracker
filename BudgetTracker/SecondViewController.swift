c559dbf14a90f1c7dd228c10d70f768d4417dcc6
Kelly Galakatos <kellygalakatos@kellys-macbook-air-2.local>
2018-12-07 04:54:04 -0600

    Initial Commit

---
+++ BudgetTracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BudgetTracker
+//
+//  Created by Kelly Galakatos on 12/7/18.
+//  Copyright © 2018 Kelly Galakatos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
